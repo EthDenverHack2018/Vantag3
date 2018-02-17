b188f1029154e814f9c07953f37d7583c67ddfe1
Kishore Bhatia <bhatia.kishore@gmail.com>
2018-02-17 11:20:01 -0700

    fixed uport issue

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,4 +1,8 @@
-import { Connect } from 'uport-connect'
+import { Connect, SimpleSigner } from 'uport-connect'
 
-export let uport = new Connect('TruffleBox')
+export const uport = new Connect('Vantag3', {
+     clientId: '2ox3yhAnwM7eahsdNwvovanM1xzRggp9gQi',
+     network: 'rinkeby',
+     signer: SimpleSigner('c33b126055d33a58a3a6e57790362417e585b8a7865c0ec9a93aa7f936e65442')
+   })
 export const web3 = uport.getWeb3()
